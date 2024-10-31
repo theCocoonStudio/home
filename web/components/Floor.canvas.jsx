22c0e828d63fdf4bd0582dd108c7922e8afe532e
Izzy Erlich <izzy@thecocoon.studio>
2024-10-31 09:47:05 -0400

    update intermediate component

--- web/components/Floor.canvas.jsx
+++ web/components/Floor.canvas.jsx
@@ -1,36 +1,18 @@
-import { MeshReflectorMaterial, useTexture } from '@react-three/drei'
-/* import colorMap from 'public/textures/metal-plate/color.jpg'
-import displacementMap from 'public/textures/metal-plate/displacement.jpg'
-import metalnessMap from 'public/textures/metal-plate/metalness.jpg'
-import normalMap from 'public/textures/metal-plate/normal.jpg'
-import roughnessMap from 'public/textures/metal-plate/roughness.jpg' */
-
-export const Floor = ({ ...props }) => {
-  /*   const textureProps = useTexture({
-    colorMap,
-    displacementMap,
-    metalnessMap,
-    normalMap,
-    roughnessMap,
-  }) */
-
+import { RigidBody } from '@react-three/rapier'
+import { forwardRef } from 'react'
+export const Floor = forwardRef(function Floor({ ...props }, ref) {
   return (
-    <mesh receiveShadow {...props}>
-      <planeGeometry args={[1, 1]} />
-      <MeshReflectorMaterial
-        blur={[300, 50]}
-        resolution={2048}
-        mixBlur={30}
-        mixStrength={90}
-        roughness={0.9}
-        depthScale={0.8}
-        minDepthThreshold={0.4}
-        maxDepthThreshold={1.4}
-        color='#202020'
-        metalness={0.75}
-        depthToBlurRatioBias={0.95}
-        /* {...textureProps} */
-      />
-    </mesh>
+    <RigidBody includeInvisible colliders='hull'>
+      <mesh
+        /* visible={false} */
+        rotation-x={-Math.PI / 2}
+        ref={ref}
+        scale={5}
+        {...props}
+      >
+        <planeGeometry args={[1, 1]} />
+        <meshBasicMaterial transparent opacity={0} />
+      </mesh>
+    </RigidBody>
   )
-}
+})
