3edbe62fbe80de9c7d0fa7f24c00c2ac3b96c0b7
Izzy Erlich <izzy@thecocoon.studio>
2024-12-02 09:24:35 -0500

    implement nav using buttongroup, refine design

--- web/pages/Layout/Nav.jsx
+++ web/pages/Layout/Nav.jsx
@@ -2,11 +2,12 @@ import { forwardRef } from 'react'
 import { AppIcon } from 'web/components/AppIcons/AppIcon'
 import CategoryFilled from '@tabler/icons-react/dist/esm/icons/IconCategoryFilled'
 import Category from '@tabler/icons-react/dist/esm/icons/IconCategory'
-import Info from '@tabler/icons-react/dist/esm/icons/IconMailFilled'
+import Info from '@tabler/icons-react/dist/esm/icons/IconMail'
 import Services from '@tabler/icons-react/dist/esm/icons/IconHeartHandshake'
 import Blog from '@tabler/icons-react/dist/esm/icons/IconArticle'
 import Vision from '@tabler/icons-react/dist/esm/icons/IconEye'
 import styles from 'web/styles/Nav.module.css'
+import { ButtonGroup } from 'web/components/ButtonGroup'
 
 export const Nav = forwardRef(function Nav({ ...props }, forwardRef) {
   return (
@@ -14,38 +15,24 @@ export const Nav = forwardRef(function Nav({ ...props }, forwardRef) {
       <div className={`${styles.navContent} content`} ref={forwardRef}>
         <div className={`${styles.navLogo}`}>izzyerlich.com</div>
         <div className={`${styles.navItems}`}>
-          <div className={`${styles.active}`}>
-            <AppIcon name='showcase' />
-          </div>
-          <div className={`${styles.starred}`}>
-            <div className={`${styles.borderLeft}`} />
-            <div className={`${styles.borderRight}`} />
-            <div>
-              <AppIcon name='vision'>
-                <Vision stroke={2} size={'60%'} />
-              </AppIcon>
+          <ButtonGroup
+            bottomLabels={false}
+            name='favourites'
+            labels={['vision', 'services', 'contact', 'blog']}
+          >
+            {/* <div className={`${styles.borderLeft}`} />
+            <div className={`${styles.borderRight}`} /> */}
+            <Vision stroke={2} size={'100%'} />
+            <Services stroke={2} size={'100%'} />
+            <Info stroke={2} size={'100%'} />
+            <Blog stroke={2} size={'100%'} />
+          </ButtonGroup>
+          <ButtonGroup name='apps' bottomLabels={false}>
+            <div className={`${styles.apps}`}>
+              <Category size='100%' className={`${styles.svg}`} />
+              <CategoryFilled size='100%' className={`${styles.svgFilled}`} />
             </div>
-            <div>
-              <AppIcon name='services'>
-                <Services stroke={2} size={'60%'} />
-              </AppIcon>
-            </div>
-            <div>
-              <AppIcon name='contact'>
-                <Info stroke={2} size={'60%'} />
-              </AppIcon>
-            </div>
-            <div>
-              <AppIcon name='blog'>
-                <Blog stroke={2} size={'60%'} />
-              </AppIcon>
-            </div>
-          </div>
-          <div className={`${styles.apps}`}>
-            <Category size='120%' className={`${styles.svg}`} />
-            <CategoryFilled size='120%' className={`${styles.svgFilled}`} />
-            <div className={`${styles.label}`}>apps</div>
-          </div>
+          </ButtonGroup>
         </div>
       </div>
     </div>
