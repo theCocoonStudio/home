3fdb22d5544a0a7b241c9d3917d1b04f94813a3f
Izzy Erlich <izzy@thecocoon.studio>
2024-12-03 11:25:36 -0500

    refine 2d nav design, remove performance opacity

--- web/pages/Layout/Nav.jsx
+++ web/pages/Layout/Nav.jsx
@@ -1,5 +1,4 @@
 import { forwardRef } from 'react'
-import { AppIcon } from 'web/components/AppIcons/AppIcon'
 import CategoryFilled from '@tabler/icons-react/dist/esm/icons/IconCategoryFilled'
 import Category from '@tabler/icons-react/dist/esm/icons/IconCategory'
 import Info from '@tabler/icons-react/dist/esm/icons/IconMail'
@@ -8,12 +7,28 @@ import Blog from '@tabler/icons-react/dist/esm/icons/IconArticle'
 import Vision from '@tabler/icons-react/dist/esm/icons/IconEye'
 import styles from 'web/styles/Nav.module.css'
 import { ButtonGroup } from 'web/components/ButtonGroup'
+import Showcase from '@tabler/icons-react/dist/esm/icons/IconDeviceTvOld'
+import Home from '@tabler/icons-react/dist/esm/icons/IconHome'
 
 export const Nav = forwardRef(function Nav({ ...props }, forwardRef) {
   return (
     <div {...props} className={`${styles.nav}`}>
       <div className={`${styles.navContent} content`} ref={forwardRef}>
-        <div className={`${styles.navLogo}`}>izzyerlich.com / showcase</div>
+        <div className={`${styles.title}`}>
+          <div className={`${styles.sub}`}>
+            <ButtonGroup className={`${styles.icon}`}>
+              <Home size='100%' stroke={1} />
+            </ButtonGroup>
+            <div className={`${styles.navLogo}`}>{`izzyerlich.com`}</div>
+          </div>
+          <span>/</span>
+          <div className={`${styles.sub}`}>
+            <ButtonGroup className={`${styles.icon}`}>
+              <Showcase size='100%' stroke={1} />
+            </ButtonGroup>
+            <span>showcase</span>
+          </div>
+        </div>
         <div className={`${styles.navItems}`}>
           <ButtonGroup
             /* bottomLabels={false} */
