16b166abf242ec595a9907ad668edd1553b82bda
Izzy Erlich <izzy@thecocoon.studio>
2025-03-12 14:23:57 -0400

    add FluidBackground component and centralize to only one useMarkupBounds hook call

---
+++ website/components/FluidBackground.canvas.jsx
@@ -0,0 +1,102 @@
+import {
+  forwardRef,
+  useCallback,
+  useImperativeHandle,
+  useMemo,
+  useRef,
+} from 'react'
+import { useFluidTexture } from 'src/hooks/useFluidTexture'
+import { Vector2 } from 'three'
+import { GradientTexture } from '@react-three/drei'
+
+const _opts = {
+  poissonIterations: 32,
+  viscousIterations: 32,
+  forceValue: 40,
+  resolution: 0.5,
+  forceSize: 90,
+  viscosity: 80,
+  dt: 0.014,
+  isViscous: true,
+  BFECC: true,
+  isBounce: true,
+}
+
+export const FluidBackground = forwardRef(function FluidBackground(
+  { stencil = {}, ...props },
+  forwardedRef,
+) {
+  const mesh = useRef()
+
+  const center = useRef(new Vector2(0, 0))
+
+  const forceCallback = useMemo(() => {
+    return (delta, elapsedTime) => {
+      const force = new Vector2(
+        Math.cos(elapsedTime),
+        Math.sin(elapsedTime),
+      ).multiplyScalar(0.5)
+      return { force, center: center.current }
+    }
+  }, [])
+
+  const options = useMemo(() => {
+    const {
+      poissonIterations: iterations_poisson,
+      viscousIterations: iterations_viscous,
+      forceValue: mouse_force,
+      resolution,
+      forceSize: cursor_size,
+      viscosity: viscous,
+      dt,
+      isViscous,
+      BFECC,
+      isBounce,
+    } = _opts
+    return {
+      iterations_poisson,
+      iterations_viscous,
+      mouse_force,
+      resolution,
+      cursor_size,
+      viscous,
+      dt,
+      isViscous,
+      BFECC,
+      isBounce,
+      /* forceCallback, */
+    }
+  }, [])
+
+  const texture = useFluidTexture(options)
+
+  const boundsCallback = useCallback(({ min, max }) => {
+    const width = max.x - min.x
+    const height = max.y - min.y
+    mesh.current.scale.setComponent(0, width)
+    mesh.current.scale.setComponent(1, height)
+  }, [])
+
+  useImperativeHandle(forwardedRef, () => ({ boundsCallback }), [
+    boundsCallback,
+  ])
+
+  return (
+    <mesh ref={mesh} {...props}>
+      <planeGeometry args={[1, 1]} />
+      <meshBasicMaterial
+        transparent
+        alphaMap={texture}
+        opacity={1.0}
+        {...stencil}
+      >
+        <GradientTexture
+          attach='map'
+          stops={[0, 0.5, 1]} // As many stops as you want
+          colors={['red', 'green', 'blue']} // Colors need to match the number of stops
+          size={1024} // Size is optional, default = 1024
+        />
+      </meshBasicMaterial>
+    </mesh>
+  )
+})
--- website/pages/Home.canvas.jsx
+++ website/pages/Home.canvas.jsx
@@ -10,25 +10,49 @@ import {
   HueSaturation,
   Vignette,
 } from '@react-three/postprocessing'
+import { FluidBackground } from '../components/FluidBackground.canvas'
+import { useMarkupBounds } from 'src/hooks/useBounds/useMarkupBounds'
+import { TitleText } from '../components/TitleText.canvas'
+import { useCallback, useRef } from 'react'
 
 export const Home = () => {
   const { colors } = useTheme()
 
+  const bg = useRef()
+  const title = useRef()
+
+  const callback = useCallback((...args) => {
+    bg.current?.boundsCallback && bg.current.boundsCallback(...args)
+    title.current?.boundsCallback && title.current.boundsCallback(...args)
+  }, [])
+
+  useMarkupBounds(
+    {
+      distance: 1,
+      callback,
+    },
+    [],
+  )
   return (
-    <ScrollControls pages={3} enabled={true} damping={0}>
-      <Panels />
-      <color attach='background' args={[colors.white]} />
-      {/* <fog attach='fog' args={['#000', 0.95, 1.5]} /> */}
-      <PerspectiveCamera makeDefault position-z={1} fov={50} />
-      <Environment preset='warehouse' />
-      <EffectComposer disableNormalPass multisampling={8}>
+    <>
+      <ScrollControls pages={3} enabled={true} damping={0}>
+        <Panels />
+        <FluidBackground ref={bg} stencil={title.current?.stencil} />
+        <TitleText fontSize={0.3} ref={title} mask={true} />
+      </ScrollControls>
+
+      {/* <EffectComposer disableNormalPass multisampling={8}>
         <Vignette
           offset={0.5} // vignette offset
           darkness={0.6} // vignette darkness
           eskil={false} // Eskil's vignette technique
         />
-      </EffectComposer>
+      </EffectComposer> */}
+      <color attach='background' args={[colors.black]} />
+      {/* <fog attach='fog' args={['#000', 0.95, 1.5]} /> */}
+      <PerspectiveCamera makeDefault position-z={1} fov={50} />
+      <Environment preset='park' />
       {/* <OrbitControls /> */}
-    </ScrollControls>
+    </>
   )
 }
