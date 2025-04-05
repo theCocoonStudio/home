46b8e0437274dfcbcfc7e87fe592a205b3acdae8
Izzy Erlich <izzy@thecocoon.studio>
2025-04-05 13:50:00 -0400

    refine dragorb animation

--- website/components/DragOrb.canvas.jsx
+++ website/components/DragOrb.canvas.jsx
@@ -22,7 +22,14 @@ import { createSphereTwistBox } from '../utils/geometry'
 import { useTheme } from '../hooks/useTheme'
 
 export const DragOrb = forwardRef(function DragOrb(
-  { orbSize = 90, forceSize = 1, forceFactor = 1, range = [0.01, 0.19] },
+  {
+    orbSize = 90,
+    idleOrbSize = 45,
+    forceSize = 1,
+    forceFactor = 1,
+    range0 = [0.01, 0.2 - 0.01],
+    range1 = [0.2, 0.3 - 0.2],
+  },
   forwardedRef,
 ) {
   const Geometry = useMemo(() => {
@@ -30,11 +37,15 @@ export const DragOrb = forwardRef(function DragOrb(
     return geometry
   }, [])
 
-  useEffect(() => () => {
-    Geometry.dispose()
-  })
+  useEffect(
+    () => () => {
+      Geometry.dispose()
+    },
+    [Geometry],
+  )
 
   const {
+    colors: { black },
     lengths: { navHeight, footerHeight },
   } = useTheme()
 
@@ -49,12 +60,17 @@ export const DragOrb = forwardRef(function DragOrb(
   const centerDiff = useRef(new Vector2(0, 0))
 
   const scale = useRef(new Vector3(1.0, 1.0, 1.0))
+
   const orbPosition = useRef(new Vector3(0, 0, 0))
 
   const line1 = useRef(new LineCurve(new Vector2(), new Vector2()))
   const line2 = useRef(new LineCurve(new Vector2(), new Vector2()))
+  const line3 = useRef(new LineCurve(new Vector2(), new Vector2()))
+  const line4 = useRef(new LineCurve(new Vector2(), new Vector2()))
+  const line5 = useRef(new LineCurve(new Vector2(), new Vector2()))
   const arc = useRef(new EllipseCurve())
   const path = useRef(new CurvePath())
+  const path1 = useRef(new CurvePath())
 
   const boundsCallback = useCallback(
     ({ min, max, ppwu }) => {
@@ -64,24 +80,32 @@ export const DragOrb = forwardRef(function DragOrb(
         (0.5 * orbSize) / ppwu.x,
         (0.5 * orbSize) / ppwu.x,
       )
+
       orbPosition.current.set(
         min.x + (max.x - min.x) / 2,
         min.y + (max.y - min.y) / 2,
         0,
       )
-      mesh.current.position.copy(orbPosition.current)
-      mesh.current.scale.copy(scale.current)
-      // orb scroll-path
+      // orb scroll-path range 0
       const _radius =
         0.5 * Math.min(max.x - min.x, max.y - min.y) -
         (navHeight + footerHeight) / ppwu.y
       line1.current.v2.setY(-_radius)
       line2.current.v1.setY(-_radius)
+      line2.current.v2.setY(max.y - 3 * scale.current.y)
       arc.current.xRadius = _radius
       arc.current.yRadius = _radius
       arc.current.aStartAngle = 2 * (3 / 4) * Math.PI
       arc.current.aEndAngle = 2 * (3 / 4) * Math.PI + 2 * Math.PI
       path.current.curves = [line1.current, arc.current, line2.current]
+      // orb scroll-path range 1
+      line3.current.v1.setY(max.y - 3 * scale.current.y)
+      line3.current.v2.setX(min.x + 4 * scale.current.x)
+      line4.current.v1.setX(min.x + 4 * scale.current.x)
+      line4.current.v2.setX(max.x - 4 * scale.current.x)
+      line5.current.v1.setX(max.x - 4 * scale.current.x)
+      line5.current.v2.setY(max.y - 3 * scale.current.y)
+      path1.current.curves = [line3.current, line4.current, line5.current]
     },
     [footerHeight, navHeight, orbSize],
   )
@@ -117,10 +141,6 @@ export const DragOrb = forwardRef(function DragOrb(
   const morphTargetInfluences = useRef([0, 0])
   const twist = useRef(true)
 
-  useEffect(() => {
-    drag.current.z = mesh.current.scale.z / 2
-  }, [])
-
   useFrame(({ clock }, delta) => {
     damp3(group.current?.position, drag.current, 0.1, delta)
 
@@ -135,29 +155,52 @@ export const DragOrb = forwardRef(function DragOrb(
 
   const scrollCallback = useCallback(
     (state, delta, scroll) => {
-      // go right
-      const offset = scroll.range(...range)
-      const visible = scroll.visible(...range)
-      const point = path.current.getPointAt(offset)
-      twist.current = !visible && offset < range[1] ? true : false
+      // choose correct path
+      const offset0 = scroll.range(...range0)
+      const offset1 = scroll.range(...range1)
+      const offset = offset1 > 0 ? offset1 : offset0
+
+      const rotation0 = scroll.curve(...range0)
+      const rotation1 = scroll.curve(...range1)
+      const rotation = offset1 > 0 ? rotation1 : rotation0
+
+      const _path = offset1 > 0 ? path1 : path
+      const point = _path.current.getPointAt(offset)
+
+      // set twist
+      twist.current = offset0 < range0[0] || offset1 > 0.99 ? true : false
+      // morph
+      morphTargetInfluences.current[0] = offset0
+      // move
+
       damp3(
         mesh.current?.position,
         [point.x, point.y, mesh.current?.position.z],
         0.0,
         delta,
       )
+      // rotate
       damp3(
         mesh.current?.rotation,
-        [Math.PI * offset, Math.PI * offset, 2 * Math.PI * offset],
-        0.15,
+        [
+          2 * Math.PI * rotation,
+          2 * Math.PI * rotation,
+          2 * Math.PI * rotation,
+        ],
+        0.05,
         delta,
       )
-
-      if (offset > 0 && visible) {
-        morphTargetInfluences.current[0] = offset
-      }
+      //scale
+      damp3(
+        mesh.current?.scale,
+        offset1 >= 0.99
+          ? scale.current.clone().multiplyScalar(idleOrbSize / orbSize)
+          : scale.current,
+        offset1 > 0.99 ? 0.15 : 0,
+        delta,
+      )
     },
-    [range],
+    [idleOrbSize, orbSize, range0, range1],
   )
 
   useImperativeHandle(
@@ -174,18 +217,16 @@ export const DragOrb = forwardRef(function DragOrb(
     <DragControls
       autoTransform={false}
       onDrag={(localMatrix) => {
-        drag.current
-          .setFromMatrixPosition(localMatrix)
-          .setZ((mesh.current?.scale.z / 2) * Math.sqrt(2))
+        drag.current.setFromMatrixPosition(localMatrix)
       }}
       onDragEnd={() => {
-        drag.current.set(0, 0, (mesh.current?.scale.z / 2) * Math.sqrt(2))
+        drag.current.set(0, 0, 0)
       }}
       dragConfig={{
         preventScroll: true,
       }}
     >
-      <group ref={group} position-z={drag.current.z}>
+      <group ref={group}>
         <mesh
           morphTargetInfluences={morphTargetInfluences.current}
           onPointerDown={() => {
@@ -203,11 +244,11 @@ export const DragOrb = forwardRef(function DragOrb(
           <meshBasicMaterial toneMapped={false} />
           <Wireframe
             fillMix={0}
-            fill='white'
+            fill={'white'}
             fillOpacity={0}
             thickness={0.4}
             strokeOpacity={1}
-            stroke='black'
+            stroke={black}
             squeeze
             squeezeMin={0.15}
             squeezeMax={1.1}
