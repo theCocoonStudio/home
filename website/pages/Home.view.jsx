9710d00293985173cb88ce989792a8892da9053a
Izzy Erlich <izzy@thecocoon.studio>
2025-04-05 13:49:43 -0400

    add scroll info arg to bounds callbacks

--- website/pages/Home.view.jsx
+++ website/pages/Home.view.jsx
@@ -4,12 +4,12 @@ import { Environment } from '@react-three/drei'
 import { useTheme } from 'website/hooks/useTheme'
 import { FluidBackground } from '../components/FluidBackground.canvas'
 import { useMarkupBounds } from 'src/hooks/useBounds/useMarkupBounds'
-import { TitleText } from '../components/TitleText.canvas'
 import { useCallback, useRef } from 'react'
 import { Performance } from '../components/Performance.canvas'
 import { DragOrb } from '../components/DragOrb.canvas'
 import { useFrame } from '@react-three/fiber'
 import { SetScroll } from '../components/SetScroll.canvas'
+import { Title } from '../components/Title.canvas'
 
 export const Home = () => {
   const { colors } = useTheme()
@@ -18,22 +18,21 @@ export const Home = () => {
   const title = useRef()
   const orb = useRef()
 
-  const callbackAt1 = useCallback((...args) => {
-    bg.current?.boundsCallback && bg.current.boundsCallback(...args)
-    title.current?.boundsCallback && title.current.boundsCallback(...args)
-    orb.current?.boundsCallback && orb.current.boundsCallback(...args)
-  }, [])
-
-  const callbackAtHalf = useCallback((...args) => {
-    title.current?.boundsCallback && title.current.boundsCallback(...args)
-  }, [])
-
   const data = useScroll()
+  const callbackAt1 = useCallback(
+    ({ ...args }) => {
+      const _args = { ...args, scroll: data }
+      bg.current?.boundsCallback && bg.current.boundsCallback(_args)
+      title.current?.boundsCallback && title.current.boundsCallback(_args)
+      orb.current?.boundsCallback && orb.current.boundsCallback(_args)
+    },
+    [data],
+  )
 
   useMarkupBounds(
     {
-      distance: [1, 0.5],
-      callback: [callbackAt1, callbackAtHalf],
+      distance: [1],
+      callback: [callbackAt1],
     },
     [],
   )
@@ -51,14 +50,12 @@ export const Home = () => {
       {/* <Panels /> */}
       <FluidBackground
         ref={bg}
-        /* stencil={title.current?.stencil} */
         forceCallback={orb.current?.forceCallback}
         colors={colors}
       />
       <DragOrb ref={orb} />
-      <SetScroll event='showScroll' rangeMin={0.2} />
-      <TitleText fontSize={60} ref={title} mask={false} text={`about`} />
-
+      <SetScroll event='showScroll' rangeMin={0.01} />
+      <Title ref={title} />
       <color attach='background' args={[colors.white]} />
       <Performance />
       <PerspectiveCamera makeDefault position-z={1} fov={30} />
