86969dac165281b1b56789e4e84b6031b5af0fa5
Izzy Erlich <izzy@thecocoon.studio>
2025-10-14 07:33:48 -0400

    allow multiple target items with keyed state

--- website/pages/Home/useTargetItems.jsx
+++ website/pages/Home/useTargetItems.jsx
@@ -1,14 +1,26 @@
 import { useContext, useEffect } from 'react'
 import { TargetItemContext } from './TargetItemContext'
 
-export const useTargetItems = (items) => {
+export const useTargetItems = (items, key) => {
   const { targetItems, setTargetItems } = useContext(TargetItemContext)
 
+  // set target items with key
   useEffect(() => {
-    if (items) {
-      setTargetItems(items)
+    if (items && key) {
+      setTargetItems((prev) => ({ ...prev, [key]: items }))
     }
-  }, [items, setTargetItems])
+  }, [items, key, setTargetItems])
 
+  // remove keyed items on unmount or key change
+  useEffect(
+    () => () => {
+      setTargetItems((prev) => {
+        const removed = { ...prev }
+        delete removed[key]
+        return removed
+      })
+    },
+    [key, setTargetItems],
+  )
   return targetItems
 }
