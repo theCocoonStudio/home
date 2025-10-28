76cf128eef5c2d50037d34a2cdd8978fe7b3814e
Izzy Erlich <izzy@thecocoon.studio>
2025-10-28 09:04:01 -0400

    add onFinish to TerminalText api; refine style

--- website/components/TerminalText.jsx
+++ website/components/TerminalText.jsx
@@ -1,10 +1,12 @@
-import { useLayoutEffect, useRef } from 'react'
+import { useCallback, useLayoutEffect, useRef } from 'react'
 import styles from 'website/styles/TerminalText.module.css'
 import { useTheme } from 'website/hooks/useTheme'
 
 export const TerminalText = ({
-  children = 'scroll up and down...',
-  typingTime = 2500,
+  children,
+  steps,
+  onFinish,
+  typingTime = 2400,
   caretTime = 900,
 }) => {
   const {
@@ -12,45 +14,70 @@ export const TerminalText = ({
   } = useTheme()
 
   const terminal = useRef()
-  useLayoutEffect(() => {
-    // keyframe effects
+
+  // animation callbacks
+  const getTypingAnimation = useCallback((_time, _steps) => {
     const typingEffect = new KeyframeEffect(
       terminal.current,
       [{ width: '0%' }, { width: '100%' }],
       {
         // keyframe options
-        duration: typingTime,
-        easing: `steps(${children.length}, end)`,
+        duration: _time,
+        easing: `steps(${_steps}, end)`,
       },
     )
-    const caretEffect = new KeyframeEffect(
-      terminal.current,
-      [
-        { borderRightColor: 'transparent' },
-        { borderRightColor: white },
-        { borderRightColor: 'transparent' },
-      ],
-      {
-        // keyframe options
-        duration: caretTime,
-        easing: 'steps(2, end)',
-        iterations: 'Infinity',
-      },
-    )
+    return new Animation(typingEffect, document.timeline)
+  }, [])
 
-    // animations
-    const typingAnimation = new Animation(typingEffect, document.timeline)
-    const caretAnimation = new Animation(caretEffect, document.timeline)
+  const getCaretAnimation = useCallback(
+    (_time) => {
+      const caretEffect = new KeyframeEffect(
+        terminal.current,
+        [
+          { borderRightColor: 'transparent' },
+          { borderRightColor: white },
+          { borderRightColor: 'transparent' },
+        ],
+        {
+          // keyframe options
+          duration: _time,
+          easing: 'steps(2, end)',
+          iterations: 'Infinity',
+        },
+      )
+      return new Animation(caretEffect, document.timeline)
+    },
+    [white],
+  )
 
-    // play animations
-    typingAnimation.play()
+  // trigger animations
+  useLayoutEffect(() => {
+    const caretAnimation = getCaretAnimation(caretTime)
     caretAnimation.play()
-    return () => {
-      // cancel previous animations
-      typingAnimation.cancel()
-      caretAnimation.cancel()
+
+    const typingAnimation = getTypingAnimation(
+      typingTime,
+      steps || children.length,
+    )
+    // set onFinish when animation completes
+    if (onFinish) {
+      typingAnimation.onfinish = onFinish
     }
-  }, [caretTime, children, typingTime, white])
+    typingAnimation.play()
+    return () => {
+      // cancel running animations on unmount
+      caretAnimation.cancel()
+      typingAnimation.cancel()
+    }
+  }, [
+    caretTime,
+    children,
+    getCaretAnimation,
+    getTypingAnimation,
+    onFinish,
+    steps,
+    typingTime,
+  ])
 
   return (
     <div ref={terminal} className={`${styles.container} space-mono-regular`}>
